899f4440298935f4dd7aec0db7309131ab07bd5d
Joshua <meraki.dot@gmail.com>
2020-01-25 16:56:08 -0800

    Updates

---
+++ precache-manifest.c93a792f385329eaa49042de799bbe4e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21a1fdc6791a2372e28e233250a6418",
+    "url": "/DollfacePMU/index.html"
+  },
+  {
+    "revision": "89c7ebd2350a9d78fe38",
+    "url": "/DollfacePMU/static/css/main.89929f8c.chunk.css"
+  },
+  {
+    "revision": "0e3e7d16826ac8794cf5",
+    "url": "/DollfacePMU/static/js/2.fc811c2b.chunk.js"
+  },
+  {
+    "revision": "f34946825965aa84dde89f731dfb7db4",
+    "url": "/DollfacePMU/static/js/2.fc811c2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89c7ebd2350a9d78fe38",
+    "url": "/DollfacePMU/static/js/main.f20283fe.chunk.js"
+  },
+  {
+    "revision": "c74efa7cc375e3e1a195",
+    "url": "/DollfacePMU/static/js/runtime-main.4497c75f.js"
+  },
+  {
+    "revision": "a64cae4b239fb425d7953a05f30c2a60",
+    "url": "/DollfacePMU/static/media/dollface.a64cae4b.svg"
+  },
+  {
+    "revision": "416a763c31d0bc7cc5368f706ea02ad5",
+    "url": "/DollfacePMU/static/media/dollface1.416a763c.JPG"
+  },
+  {
+    "revision": "bc16a06704f8b0dee25a73024a6b2eff",
+    "url": "/DollfacePMU/static/media/dollface2.bc16a067.JPG"
+  },
+  {
+    "revision": "58291cebd676b6da83a17afbafcda5ce",
+    "url": "/DollfacePMU/static/media/dollface3.58291ceb.JPG"
+  },
+  {
+    "revision": "70f7463bc17978812fc73372a66715d0",
+    "url": "/DollfacePMU/static/media/facebook.70f7463b.svg"
+  },
+  {
+    "revision": "d0b7c68504cafaaa8a95db96595f101a",
+    "url": "/DollfacePMU/static/media/instagram.d0b7c685.svg"
+  },
+  {
+    "revision": "8cc95c3acc548aa98f2e3b47e36813a3",
+    "url": "/DollfacePMU/static/media/steph_profile.8cc95c3a.jpg"
+  },
+  {
+    "revision": "4b66644970fac8f61ad3a44f6041f419",
+    "url": "/DollfacePMU/static/media/yelp.4b666449.svg"
+  }
+]);
